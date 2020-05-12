c6761d5cc04b13e66086bc5320d23aee5f90061f
ThinkWriteFly <65169006+ThinkWriteFly@users.noreply.github.com>
2020-05-12 19:44:52 +0100

    Initial commit

---
+++ William_Carlos__Williams__Bot.py
@@ -0,0 +1,31 @@
+import random
+# this is william carlos williams bot
+# infinite nonsense variations of 'this is just to say'
+
+
+
+# line structure
+Line_1 = "\t I have " + random.choice() + "\n"
+Line_2 = "\t the " + random.choice() + "\n"
+Line_3 = "\t that were " + random.choice() +"\n"
+Line_4 = "\t the " + random.choice() + "\n" 
+Line_5 = "\t and which \n"
+Line_6 = "\t you were probably\n"
+Line_7 = "\t saving\n"
+Line_8 = "\t for " + random.choice() + "\n"
+Line_9 = "\t Forgive me \n"
+Line_10 = "\t they were" + random.choice() + "\n"
+Line_11 = "\t so" + random.choice() + "\n"
+Line_12 = "\t and so" + random.choice() + "\n"
+
+# poem structure
+poem = Line_1 + Line_2 + Line_3 + Line_4 + " \n" + Line_5 + Line_6 + Line_7 + " \n" + Line_8 + "\n" + Line_9 + Line_10 + Line_11 + Line_12
+
+#Lists
+Noun_Object_plural =[chairs,peaches, plums,]
+Past_simlple_Verb =[eaten,slapped,taken,diluted,defected,]
+Meal_Occasion = [breakfast,samhein,easter,dinner,banquet]
+# Output structure
+print (" ThIs Is JuSt To SaY \n\n\n\n")
+print (poem)
+print ("\n\n\t\t\t - WcWBot")
